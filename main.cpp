fe14368153c04b935033c67c51b3cae9df708456
andrei12225 <andrei12225@outlook.com>
2024-11-15 15:33:03 +0200

    Rezolvare prim curs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -10,23 +11,38 @@ int main()
 
     int no_problems, no_doctors;
     string name, speciality;
-    
+    vector<string> problems;
+    vector<string> names;
+    vector<string> doctors;
+
     inFile >> no_problems;
 
-    for (int i = 0; i < no_problems; i++)
-    {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+    for (int i = 0; i < no_problems; ++i) {
+        inFile >> name >> speciality;
+        problems.push_back(speciality);
+        names.push_back(name);
     }
 
     inFile >> no_doctors;
 
-    for (int i = 0; i < no_doctors; i++)
-    {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+    for (int i = 0; i < no_doctors; ++i) {
+        inFile >> name >> speciality;
+        doctors.push_back(speciality);
+    }
+
+    for (int i = 0; i < problems.size(); ++i) {
+        int ok = 0;
+        for (const auto & doctor : doctors) {
+            if (problems[i] == doctor) {
+                ok = 1;
+            }
+        }
+
+        if (ok == 1) {
+            cout<<names[i]<<" Acceptat"<<'\n';
+        } else {
+            cout<<names[i]<<" Respins"<<'\n';
+        }
     }
 
     return 0;
